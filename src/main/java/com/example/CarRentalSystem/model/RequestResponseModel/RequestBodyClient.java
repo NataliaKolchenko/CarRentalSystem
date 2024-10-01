c29fe8aaca89445ba1cbf5ad90f3076607bcc8be
Natalia <kolchenko.nata.yu@gmail.com>
2024-10-01 17:53:41 +0200

    created model classes RequestBodyClient, ResponseAPI

---
+++ src/main/java/com/example/CarRentalSystem/model/RequestResponseModel/RequestBodyClient.java
@@ -0,0 +1,4 @@
+package com.example.CarRentalSystem.model.RequestResponseModel;
+
+public class RequestBodyClient {
+}
---
+++ src/main/java/com/example/CarRentalSystem/model/RequestResponseModel/ResponseAPI.java
@@ -0,0 +1,8 @@
+package com.example.CarRentalSystem.model.RequestResponseModel;
+
+public class ResponseAPI {
+    public Object response;
+
+    public ResponseAPI() {
+    }
+}
