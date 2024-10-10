02301d4d11d563489abe8f4087448a8213f8e05f
Natalia <kolchenko.nata.yu@gmail.com>
2024-10-10 19:48:49 +0200

    created model "Model" and configure liquibase for it

---
+++ src/main/java/com/example/CarRentalSystem/model/Model.java
@@ -0,0 +1,34 @@
+package com.example.CarRentalSystem.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    @NotBlank(message = "modelName may not be blank or null or has spaces")
+    private String modelName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "brand_id")
+    @NotNull(message = "Brand may not be null")
+    private Brand brand;
+
+    public Model() {
+    }
+
+    public Model(String modelName, Brand brand) {
+        this.modelName = modelName;
+        this.brand = brand;
+    }
+}
