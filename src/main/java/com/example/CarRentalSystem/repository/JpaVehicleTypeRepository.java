d2b94ab462e41fd9a144845af2169b1569dae3c5
Natalia <kolchenko.nata.yu@gmail.com>
2024-10-15 12:13:45 +0200

    added JpaVehicleTypeRepository

---
+++ src/main/java/com/example/CarRentalSystem/repository/JpaVehicleTypeRepository.java
@@ -0,0 +1,7 @@
+package com.example.CarRentalSystem.repository;
+
+import com.example.CarRentalSystem.model.VehicleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaVehicleTypeRepository extends JpaRepository<VehicleType, Long> {
+}
