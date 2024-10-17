daaa30043a1168f69397736e1955b69d764f016f
Natalia <kolchenko.nata.yu@gmail.com>
2024-10-17 22:29:08 +0200

    added Branch class

---
+++ src/main/java/com/example/CarRentalSystem/model/Branch.java
@@ -0,0 +1,48 @@
+package com.example.CarRentalSystem.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Branch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private String id;
+
+    @Column
+    @NotBlank(message = "BranchName may not be blank")
+    private String branchName;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id")
+    @NotNull(message = "Address may not be null")
+    private Address address;
+
+    @Column
+    @NotBlank(message = "Phone may not be blank")
+    private String phone;
+
+    @Column
+    @NotBlank(message = "WorkingTime may not be blank")
+    private String workingTime;
+
+    public Branch() {
+    }
+
+    public Branch(String branchName, Address address, String phone, String workingTime) {
+        this.branchName = branchName;
+        this.address = address;
+        this.phone = phone;
+        this.workingTime = workingTime;
+    }
+
+    public Branch(String branchName) {
+        this.branchName = branchName;
+    }
+}
