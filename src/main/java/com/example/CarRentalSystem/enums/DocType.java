a0dee22fb78eae6c55a59ed763c1f521f75a1e34
Natalia <kolchenko.nata.yu@gmail.com>
2024-10-23 14:03:01 +0200

    created enums

---
+++ src/main/java/com/example/CarRentalSystem/enums/DocType.java
@@ -0,0 +1,8 @@
+package com.example.CarRentalSystem.enums;
+
+public enum DocType {
+    USER_PASSPORT,
+    DRIVER_LICENSE,
+    AUTO_PASSPORT,
+    AUTO_REGISTRATION_CERTIFICATE,
+}
---
+++ src/main/java/com/example/CarRentalSystem/enums/TransmissionType.java
@@ -0,0 +1,6 @@
+package com.example.CarRentalSystem.enums;
+
+public enum TransmissionType {
+    MANUAL,
+    AUTOMATIC
+}
