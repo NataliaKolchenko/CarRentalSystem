d040aabb806383bed2c087aa2a3a683073d2b700
Natalia <kolchenko.nata.yu@gmail.com>
2024-10-29 16:14:53 +0100

    create vehicle and create vehicle_doc aew working , but return some null-s

--- src/main/java/com/example/CarRentalSystem/service/VehicleServiceImp.java
+++ src/main/java/com/example/CarRentalSystem/service/VehicleServiceImp.java
@@ -1,53 +1,68 @@
 package com.example.CarRentalSystem.service;
 
-import com.example.CarRentalSystem.model.Vehicle;
-import com.example.CarRentalSystem.model.VehicleDoc;
+import com.example.CarRentalSystem.enums.EngineType;
+import com.example.CarRentalSystem.enums.TransmissionType;
+import com.example.CarRentalSystem.exception.SubjectNotFoundException;
+import com.example.CarRentalSystem.model.*;
 import com.example.CarRentalSystem.repository.JpaVehicleRepository;
-import com.example.CarRentalSystem.service.interfaces.VehicleDocService;
-import com.example.CarRentalSystem.service.interfaces.VehicleService;
+import com.example.CarRentalSystem.service.interfaces.*;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Validated
 public class VehicleServiceImp implements VehicleService {
     private final JpaVehicleRepository vehicleRepository;
     private final VehicleDocServiceImp docService;
+    private final VehicleTypeServiceImp typeService;
+    private final SubTypeService subTypeService;
+    private final BrandService brandService;
+    private final ModelService modelService;
+    private final BranchService branchService;
 
-    public VehicleServiceImp(JpaVehicleRepository vehicleRepository, VehicleDocServiceImp docService) {
+    public VehicleServiceImp(JpaVehicleRepository vehicleRepository, VehicleDocServiceImp docService, VehicleTypeServiceImp typeService, SubTypeService subTypeService, BrandService brandService, ModelService modelService, BranchService branchService) {
         this.vehicleRepository = vehicleRepository;
         this.docService = docService;
+        this.typeService = typeService;
+        this.subTypeService = subTypeService;
+        this.brandService = brandService;
+        this.modelService = modelService;
+        this.branchService = branchService;
     }
 
     @Override
     public Vehicle create(Vehicle vehicle) {
-        VehicleDoc doc = docService.create(new VehicleDoc(
-                vehicle.getVehicleDocs().get(0).getVehicleDocType(),
-                vehicle.getVehicleDocs().get(0).getLink(),
-                vehicle.getVehicleDocs().get(0).getVehicle()));
-        List<VehicleDoc> docList = new ArrayList<>();
-        docList.add(doc);
+        Vehicle savedVehicle = vehicleRepository.save(vehicle);
 
-        Vehicle newVehicle = new Vehicle(
-                vehicle.getType(),
-                vehicle.getSubType(),
-                vehicle.isActive(),
-                vehicle.getBrand(),
-                vehicle.getModel(),
-                vehicle.getEngineType(),
-                vehicle.getYear(),
-                vehicle.getBranch(),
-                vehicle.getTransmissionType(),
-                vehicle.getMileage(),
-                docList,
-                vehicle.getCity(),
-                vehicle.isFavorite()
-        );
+        Long id = savedVehicle.getId();
+        VehicleType type = typeService.getById(vehicle.getType().getId());
+        SubType subType = subTypeService.getById(vehicle.getSubType().getId());
+        boolean active = savedVehicle.isActive();
+        Brand brand = brandService.getVehicleBrandById(vehicle.getBrand().getId());
+        Model model = modelService.getModelById(vehicle.getModel().getId());
+        EngineType engineType = vehicle.getEngineType();
+        int year = vehicle.getYear();
+        Branch branch = branchService.getById(vehicle.getBranch().getId());
+        TransmissionType transmissionType = vehicle.getTransmissionType();
+        int mileage = vehicle.getMileage();
+        List<VehicleDoc> vehicleDocs = savedVehicle.getVehicleDocs();
+        String city = vehicle.getCity();
+        boolean favorite = vehicle.isFavorite();
 
+        Vehicle newVehicle = new Vehicle(type, subType, active, brand, model, engineType, year, branch,
+                transmissionType, mileage, vehicleDocs, city, favorite);
+        newVehicle.setId(id);
 
-        return vehicleRepository.save(newVehicle);
+        return newVehicle;
+    }
+
+    public Vehicle getById(Long vehicleId) {
+        Optional<Vehicle> vehicleOpt = vehicleRepository.findById(vehicleId);
+        Vehicle vehicle = vehicleOpt.orElseThrow(() -> new SubjectNotFoundException("VehicleId was not wound"));
+        return vehicle;
     }
 }
