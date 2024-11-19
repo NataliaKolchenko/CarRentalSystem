311dfddfc1d98be6d21a63e91bd662ac82af92bd
Natalia <kolchenko.nata.yu@gmail.com>
2024-11-19 09:08:02 +0100

    refactor code (delete unusable classes)

--- src/main/java/com/example/CarRentalSystem/exception/IncorrectUserDocTypeException.java
+++
@@ -1,7 +0,0 @@
-package com.example.CarRentalSystem.exception;
-
-public class IncorrectUserDocTypeException extends BusinessException{
-    public IncorrectUserDocTypeException(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/com/example/CarRentalSystem/repository/JpaVehicleDocRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.example.CarRentalSystem.repository;
-//
-//import com.example.CarRentalSystem.model.VehicleDoc;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface JpaVehicleDocRepository extends JpaRepository<VehicleDoc, Long> {
-//}
