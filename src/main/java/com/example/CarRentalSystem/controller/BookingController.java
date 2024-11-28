fc062ff4a45d363589003124f9fd81a1586540d2
Natalia <kolchenko.nata.yu@gmail.com>
2024-11-28 15:48:36 +0100

    add logging

--- src/main/java/com/example/CarRentalSystem/controller/BookingController.java
+++ src/main/java/com/example/CarRentalSystem/controller/BookingController.java
@@ -7,6 +7,7 @@ import com.example.CarRentalSystem.service.interfaces.BookingService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
