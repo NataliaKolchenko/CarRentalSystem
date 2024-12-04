a7517e39fd158c104ef069edcf1cd1b9d0f0e97d
Natalia <kolchenko.nata.yu@gmail.com>
2024-12-04 16:41:02 +0100

    add JaCoCo, refactoring tests

--- src/test/java/com/example/CarRentalSystem/controller/intergationTests/SearchControllerTest.java
+++ src/test/java/com/example/CarRentalSystem/controller/intergationTests/SearchControllerTest.java
@@ -2,6 +2,7 @@ package com.example.CarRentalSystem.controller.intergationTests;
 
 import com.example.CarRentalSystem.model.enums.City;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
